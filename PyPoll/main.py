dbc0b1beaa66361e2459cf55669141e2b6f543f1
chienyuanhung <chien.yuanhung@gmail.com>
2019-05-01 22:17:30 -0700

    modify the code for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,10 +3,10 @@ import csv
 
 data_path= os.path.join('Resources','election_data.csv')
 
-candidate_list = []
-results = {}
+candidate_list = [] #list for the candudates
+results = {} #dictionary to record the ballots for each candidate
 
-#read the csv data and get candidate list
+#read the csv data, get candidate list and get the ballot
 with open(data_path, newline = '') as data_file:
     csvreader = csv.reader(data_file, delimiter = ',')
     #remove the header
@@ -14,21 +14,13 @@ with open(data_path, newline = '') as data_file:
     #get the candidate_list
     for row in csvreader:
         if not row[2] in candidate_list:
+            candidate = row[2]
             candidate_list.append(row[2])
-
-#create dictioary for results
-for candidate in candidate_list:
-    results[candidate] = 0
-
-#count the ballot
-
-with open(data_path, newline = '') as data_file:
-    csvreader = csv.reader(data_file, delimiter = ',')
-    #remove the header
-    csv_header = next(csvreader)
-    for row in csvreader:
-        candidate = row[2]
-        results[candidate] +=1
+            results[candidate] = 1
+        else:
+            candidate = row[2]
+            results[candidate] +=1
+            
 
 #get total ballots
 total_votes = 0
